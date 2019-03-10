781c018c9a984cd8b0d47b2169d3e949e6212b5b
dodohow <dodohow1011@gmail.com>
2019-03-10 23:34:36 +0800

    semi-semi-semi-final

--- hw1/1.1/1_1.py
+++ hw1/1.1/1_1.py
@@ -16,17 +16,19 @@ def main():
     from deep import Deep 
     from shallow import Shallow
     from two_layers import two_layers
-    model = Deep()
+    # model = Deep()
     # model = Shallow()
-    # model = two_layers()
+    model = two_layers()
     model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 
     history = model.fit(train_x, train_y, batch_size=16, epochs=1500)
-
+    # model.save('deep_model.h5')
+    # model.save('shallow_model.h5')
+    model.save('two_layers_model.h5')
     import pickle
-    # with open('two_layers_history.pickle', 'wb') as f:
+    with open('two_layers_history.pickle', 'wb') as f:
     # with open('shallow_history.pickle', 'wb') as f:
-    with open('deep_history.pickle', 'wb') as f:
+    # with open('deep_history.pickle', 'wb') as f:
         pickle.dump(history.history, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
