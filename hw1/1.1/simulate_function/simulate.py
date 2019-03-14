efe76e43276904556613f8b8f3be5ea14920a2fb
dodohow <dodohow1011@gmail.com>
2019-03-15 01:21:46 +0800

    90% final

--- hw1/1.1/simulate_function/simulate.py
+++ hw1/1.1/simulate_function/simulate.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
-
+import sys
 PI = math.pi
 
 def main():
@@ -16,20 +16,35 @@ def main():
     from deep import Deep 
     from shallow import Shallow
     from two_layers import two_layers
-    # model = Deep()
-    # model = Shallow()
-    model = two_layers()
+    if len(sys.argv) != 2:
+        print ('usage: python3.5 simulate.py deep/shallow/two_layers')
+        sys.exit()
+    if sys.argv[1] == 'deep':
+        model = Deep()
+    elif sys.argv[1] == 'shallow':
+        model = Shallow()
+    elif sys.argv[1] == 'two_layers':
+        model = two_layers()
     model.compile(loss='mse', optimizer='adam', metrics=['mse'])
 
     history = model.fit(train_x, train_y, batch_size=16, epochs=1500)
-    # model.save('deep_model.h5')
-    # model.save('shallow_model.h5')
-    model.save('two_layers_model.h5')
+    
+    if sys.argv[1] == 'deep':
+        model.save('deep_model.h5')
+    elif sys.argv[1] == 'shallow':
+        model.save('shallow_model.h5')
+    elif sys.argv[1] == 'two_layers':
+        model.save('two_layers_model.h5')
     import pickle
-    with open('two_layers_history.pickle', 'wb') as f:
-    # with open('shallow_history.pickle', 'wb') as f:
-    # with open('deep_history.pickle', 'wb') as f:
-        pickle.dump(history.history, f, protocol=pickle.HIGHEST_PROTOCOL)
+    if sys.argv[1] == 'deep':
+        with open('deep_history.pickle', 'wb') as f:
+            pickle.dump(history.history, f, protocol=pickle.HIGHEST_PROTOCOL)
+    elif sys.argv[1] == 'shallow':
+        with open('shallow_history.pickle', 'wb') as f:
+            pickle.dump(history.history, f, protocol=pickle.HIGHEST_PROTOCOL)
+    elif sys.argv[1] == 'two_layers':
+        with open('two_layers_history.pickle', 'wb') as f:
+            pickle.dump(history.history, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
     main()
