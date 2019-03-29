4956e9eb2b972513335444b487b3632fb9949f34
willytai43 <b04502139@ntu.edu.tw>
2019-03-29 22:15:42 +0800

    not important

--- hw1/hw1_3/sensitivity/sensitivity_keras.py
+++ hw1/hw1_3/sensitivity/sensitivity_keras.py
@@ -5,9 +5,17 @@ from tensorflow.keras.layers import Conv2D,Dense,Flatten,MaxPooling2D,BatchNorma
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
+from keras.backend.tensorflow_backend import set_session
 import tensorflow.keras.backend as K
-from sklearn.metrics import accuracy_score
 import sys
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.6
+set_session(tf.Session(config=config))
+
+
 
 def read():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -23,11 +31,11 @@ def read():
 
 def deep4():
     model = Sequential()
-    model.add(Conv2D(144, kernel_size=(3,3), activation='relu', input_shape=(28,28,1)))
-    model.add(Conv2D(144, kernel_size=(3,3), activation='relu'))
+    model.add(Conv2D(8, kernel_size=(3,3), activation='relu', input_shape=(28,28,1)))
+    model.add(Conv2D(8, kernel_size=(3,3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(144, kernel_size=(3,3), activation='relu'))
-    model.add(Conv2D(144, kernel_size=(3,3), activation='relu'))
+    model.add(Conv2D(8, kernel_size=(3,3), activation='relu'))
+    model.add(Conv2D(8, kernel_size=(3,3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Flatten())
     model.add(Dense(units=10, activation='softmax'))
@@ -40,8 +48,9 @@ def training(number):
     conv2d_1_input = tf.placeholder(tf.float32,shape=(None,28,28,1))
     model = deep4()
     with tf.Session() as sess:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "1"
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        history = model.fit(x_train,y_train,batch_size=number,epochs=30)
+        history = model.fit(x_train,y_train,batch_size=number,epochs=20)
 #    y_train = tf.convert_to_tensor(y_train)
 #    model.outputs = tf.convert_to_tensor(model.outputs)
 #    loss = tf.keras.losses.categorical_crossentropy(y_train, model.outputs)
@@ -50,18 +59,17 @@ def training(number):
         y_train = tf.convert_to_tensor(y_train)
 #        y_output = model.apply(x_test)
 #        y_test = tf.convert_to_tensor(y_test)
+        os.environ["CUDA_VISIBLE_DEVICES"] = ""
         y_output = model.apply(x_train)
         y_output = tf.convert_to_tensor(y_output)
         loss = tf.keras.losses.categorical_crossentropy(y_train, y_output)
 #        print(loss.shape)
 #        grad = tf.gradients(loss, x_test)
         grad = tf.gradients(loss, x_train)
-        grad_list = []
         for i in grad:
-            print(i.shape)
-            grad_list.append(tf.norm(i,2))
-        grad_list=sess.run(grad_list)
-        print(grad_list)
+            grad_norm = tf.norm(i,2)
+        grad_norm=sess.run(grad_norm)
+        return grad_norm
 #    x_train = tf.convert_to_tensor(x_train)
         
 #    with tf.Session() as sess:
@@ -78,9 +86,12 @@ def training(number):
 #        pickle.dump(history.history, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
-    training(10)
-    training(50)
-    training(100)
-    training(200)
-    training(500)
-    training(1000)
+    grad_list = []
+    grad_list.append(training(10))
+    grad_list.append(training(50))
+    grad_list.append(training(100))
+    grad_list.append(training(200))
+    grad_list.append(training(500))
+    grad_list.append(training(1000))
+    print(grad_list)
+    np.save("norm_model",grad_list)
