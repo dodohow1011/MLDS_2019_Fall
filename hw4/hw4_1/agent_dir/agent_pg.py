a6027d0fada4a05f60a9823fd2d991044813d0ee
willytai43 <b04502139@ntu.edu.tw>
2019-06-06 15:29:42 +0800

    function play complete

--- hw4/hw4_1/agent_dir/agent_pg.py
+++ hw4/hw4_1/agent_dir/agent_pg.py
@@ -5,14 +5,19 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 from torch.autograd import Variable
-# from torchvision import transforms
+from torchvision import transforms
 import torch.optim as optim
 from torch.distributions import Categorical
-import sys
-# from torchsummary import summary
+import sys, os
+from torchsummary import summary
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 episodes = 1000
 checkpoint = "checkpoints"
+trans = transforms.Compose([transforms.ToPILImage(),
+                            transforms.Grayscale(num_output_channels=1),
+                            transforms.ToTensor()])
 
 def prepro(o,image_size=[80,80]):
     """
@@ -31,32 +36,42 @@ def prepro(o,image_size=[80,80]):
     resized = y.thumbnail()
     return np.expand_dims(resized.astype(np.float32),axis=2)
 
-def RGB2Gray(image):
+def RGB2Gray(image, trans=trans):
     # remove score board
-    # some margins still remain
-    image = image[23:,:,:]
-    gray = np.zeros((image.shape[0], image.shape[1]))
-    for row in range(image.shape[0]):
-        for col in range(image.shape[1]):
-            r = image[row][col][0] 
-            g = image[row][col][1] 
-            b = image[row][col][2] 
-            gray[row][col] = 0.2126*r + 0.7125*g + 0.0722*b
-    return gray
+    image = image[24:,:,:]
+    image = trans(image)
+    return image
 
 class AgentModel(nn.Module):
     def __init__(self):
         super(AgentModel, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
-        self.linear = nn.Linear(32*187*160, 3)
+        self.pool  = nn.MaxPool2d(2, stride=2)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.linear = nn.Linear(256*23*20, 3)
 
     def forward(self, state):
         x = F.relu(self.conv1(state))
-        x = x.reshape(1, -1)
+        x = self.pool(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+
+        x = x.view(x.size(0), -1)
         x = self.linear(x)
         x = F.softmax(x, dim=1)
         return x
+
+    def initWeight(self, mean=0.0, std=0.02):
+        for m in self._modules:
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                m.weight.data.normal_(mean, std)
+                m.bias.data.zero_()
 
 class Agent_PG(Agent):
     def __init__(self, env, args):
@@ -67,17 +82,18 @@ class Agent_PG(Agent):
 
         super(Agent_PG,self).__init__(env)
 
+        self.env = env
+        self.policy = AgentModel().cuda()
+        self.data = []
+
+        # summary(self.policy, (1, 186, 160))
+        # sys.exit()
+
         if args.test_pg:
             #you can load your model here
             print('loading trained model')
-
-        ##################
-        # YOUR CODE HERE #
-        ##################
-        self.env = env
-        self.policy = AgentModel()
-        self.rewards = []
-        self.loss = []
+        else:
+            self.policy.initWeight()
 
     def init_game_setting(self):
         """
@@ -100,31 +116,41 @@ class Agent_PG(Agent):
         # YOUR CODE HERE #
         ##################
         optimizer = optim.RMSprop(self.policy.parameters(), lr=1e-4, weight_decay=0.99)
-        total_reward = 0
         for episode in range(episodes):
-            state = self.env.reset()
-            while(True):
-                state = RGB2Gray(state)
-                state = state.reshape(1, state.shape[0], -1) # size = (1, 187, 160)
-                action = self.make_action(state)
-
-                action = action + 1 # action space {1, 2, 3}
-                
-                state, reward, done, _ = self.env.step(action)
-                total_reward = total_reward + reward
-
-                self.rewards.append(reward) # save for update
-
-                if done:
-                    print ("Episode{} done! Reward: {:3f}".format(episode+1, total_reward))
-                    total_reward = 0
-                    break
+            self.play()
+            self.update()
 
             if  (episode+1) % 50 == 0:
                 print ("Save checkpoint")
                 torch.save(self.policy.state_dict(), os.path.join(checkpoints, "_{}.pt".format(episode+1)))
 
+    def play(self):
+        # clear memory
+        self.data = []
+        state = self.env.reset()
+        total_reward = 0
+        while(True):
+            state = RGB2Gray(state)
+            action = self.make_action(state)
 
+            action = action + 1 # action space {1, 2, 3}
+               
+            next_state, reward, done, _ = self.env.step(action)
+            total_reward = total_reward + reward
+
+            self.data.append((state, action, reward)) # save for update
+            print ('action', action)
+
+            state = next_state
+
+            if done:
+                print ("Episode {} done! Reward: {:3f}".format(episode+1, total_reward))
+                print (len(self.data))
+                sys.exit()
+                break
+
+    def update(self):
+        pass
 
     def make_action(self, observation, test=True):
         """
@@ -137,15 +163,20 @@ class Agent_PG(Agent):
         Return:
             action: int
                 the predicted action from trained model
+
+        There are six actions: 0 ~ 5
+        0,1: Stop
+        2,4: Up
+        3,5: Down
         """
         ##################
         # YOUR CODE HERE #
         ##################
-        state = torch.from_numpy(observation).float().unsqueeze(0) # size = (1, 1, 187, 160)
+        state = observation.unsqueeze(0) # size = (1, 1, 187, 160)
+        state = state.cuda()
         probs = self.policy(state)
         m = Categorical(probs)
         action = m.sample()
-        self.loss.append(m.log_prob(action)) # save for update
-
+        # self.loss.append(m.log_prob(action)) # save for update
         return action.item()
 
